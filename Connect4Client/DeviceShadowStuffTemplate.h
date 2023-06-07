25971c71f0146d530a89f16a7d04c14da56dc984
ian-chuang <iantc104@gmail.com>
2023-06-07 14:31:21 -0700

    Feat: Client Code working
    Issues arise when server or client is stopped during a game

---
+++ Connect4Client/DeviceShadowStuffTemplate.h
@@ -0,0 +1,18 @@
+/*
+ * DeviceShadowStuff.h
+ *
+ *  Created on: Jun 7, 2023
+ *      Author: iantc
+ */
+
+#ifndef DEVICESHADOWSTUFF_H_
+#define DEVICESHADOWSTUFF_H_
+
+#define SERVER_NAME "<Server Name Here>"
+
+#define GETHEADER "GET /things/<Thing Name>/<Shadow Name> HTTP/1.1\n\r"
+#define POSTHEADER "POST /things/<Thing Name>/<Shadow Name> HTTP/1.1\n\r"
+#define HOSTHEADER "Host: <Server Name>\r\n"
+
+
+#endif /* DEVICESHADOWSTUFF_H_ */
